a3600fbd1d268a2f46b268c6ffc45c581db9b8d2
buuraak77 <brkylmz8@hotmail.com>
2022-06-28 20:28:32 +0300

    Initial Commit

---
+++ Challenge2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge2
+//
+//  Created by Burak Yılmaz on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
